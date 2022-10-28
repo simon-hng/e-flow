3e99471d180f36b662a49c7511196a145e53047f
Simon Huang <simon.huang@tum.de>
2022-10-28 21:39:15 +0200

    improve job report

--- pages/jobreport.tsx
+++ pages/jobreport.tsx
@@ -1,15 +1,11 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBolt } from "@fortawesome/free-solid-svg-icons";
 import Table from "../components/table";
 
 function JobReport() {
   return (
- 
-  <div className="w-full col-span-2 h-64">
-        <Table />
-      </div>
-
-
+    <div className="px-4">
+      <h1 className="text-2xl font-bold pb-4">Job report</h1>
+      <Table />
+    </div>
   );
 }
 
