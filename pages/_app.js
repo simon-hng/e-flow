365fa91bc52f32f8a492b3807a56218975252e2a
Simon Huang <simon.huang@tum.de>
2022-10-28 15:42:34 +0200

    add fontawesome

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faWind } from "@fortawesome/free-solid-svg-icons";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <header className="p-4">e-Flow</header>
+      <header className="p-4">
+        e-Flow
+        <FontAwesomeIcon icon={faWind} className="pl-2" />
+      </header>
       <Component {...pageProps} />
     </>
   );
