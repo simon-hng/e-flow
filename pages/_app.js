8a65aef51039bc0b2fdbbf2190e0bb887aceb5f6
Simon Huang <simon.huang@tum.de>
2022-10-28 17:42:44 +0200

    header

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBolt } from "@fortawesome/free-solid-svg-icons";
+import { faBolt, faPlus } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
 import "react-datepicker/dist/react-datepicker.css";
 import "../styles/globals.css";
@@ -11,37 +11,14 @@ function MyApp({ Component, pageProps }) {
         <div className="flex-1">
           <Link href="/" className="btn btn-ghost normal-case text-xl">
             eFlow
-            <FontAwesomeIcon icon={faBolt} className="inline-block w-6 pl-2" />
+            <FontAwesomeIcon icon={faBolt} className="inline-block h-4 pl-2" />
           </Link>
         </div>
         <div className="flex-none">
-          <ul className="menu menu-horizontal p-0">
-            <li>
-              <a>Overview</a>
-            </li>
-            <li tabIndex={0}>
-              <a>
-                Jobs
-                <svg
-                  className="fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-                </svg>
-              </a>
-              <ul className="p-2 bg-base-100">
-                <li>
-                  <a>Run</a>
-                </li>
-                <li>
-                  <a>Schedule</a>
-                </li>
-              </ul>
-            </li>
-          </ul>
+          <Link href="/create-job" className="btn rounded-full">
+            new job{" "}
+            <FontAwesomeIcon icon={faPlus} className="inline-block h-4 ml-2" />
+          </Link>
         </div>
       </header>
 
