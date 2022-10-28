985ab9262dc7940c6936b056eedf90c678a67373
Simon Huang <simon.huang@tum.de>
2022-10-28 15:46:55 +0200

    change icon to bolt

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faWind } from "@fortawesome/free-solid-svg-icons";
+import { faBolt } from "@fortawesome/free-solid-svg-icons";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
@@ -7,7 +7,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <header className="p-4">
         e-Flow
-        <FontAwesomeIcon icon={faWind} className="pl-2" />
+        <FontAwesomeIcon icon={faBolt} className="pl-2" />
       </header>
       <Component {...pageProps} />
     </>
