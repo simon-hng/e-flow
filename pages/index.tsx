125a542299885f04f235cdb4d667ca6e57ba859d
Simon Huang <simon.huang@tum.de>
2022-10-28 16:15:59 +0200

    remove example

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,65 +1,8 @@
 import Head from "next/head";
 import Image from "next/image";
-import { useState } from "react";
-import DatePicker from "react-datepicker";
-import {
-  Area,
-  AreaChart,
-  CartesianGrid,
-  Tooltip,
-  XAxis,
-  YAxis,
-} from "recharts";
 import styles from "../styles/Home.module.css";
 
-const data = [
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
-
 export default function Home() {
-  const [startDate, setStartDate] = useState(new Date());
-
   return (
     <div className={styles.container}>
       <Head>
@@ -69,25 +12,6 @@ export default function Home() {
       </Head>
 
       <main className="min-h-screen">
-        <div className="w-16">
-          <AreaChart width={500} height={400} data={data}>
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Area
-              type="monotone"
-              dataKey="uv"
-              stroke="#8884d8"
-              fill="#8884d8"
-            />
-          </AreaChart>
-        </div>
-        <DatePicker
-          showTimeSelect
-          selected={startDate}
-          onChange={(date: Date) => setStartDate(date)}
-        />
       </main>
 
       <footer className={styles.footer}>
