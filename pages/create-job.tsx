2ab5b9724e515644490d7cc9961afa78d50d4b47
Simon Huang <simon.huang@tum.de>
2022-10-29 10:01:48 +0200

    add headline

--- pages/create-job.tsx
+++ pages/create-job.tsx
@@ -20,101 +20,105 @@ function CreateJob() {
   }, [startDate, endDate]);
 
   return (
-    <div className="flex flex-col px-4 lg:flex-row-reverse gap-4">
-      <div className="w-full col-span-2 h-80 lg:w-2/4">
-        <Preview data={previewData} />
-      </div>
+    <div className="px-4">
+      <h1 className="text-2xl font-bold pb-4">Create job</h1>
 
-      <div className="grid grid-cols-2 gap-2 w-full lg:w-2/4">
-        <div className="dropdown col-span-2">
-          <label tabIndex={0} className="btn m-1 w-full">
-            {!type ? "Select job" : type}
-            <FontAwesomeIcon
-              icon={faChevronDown}
-              className="inline-block pl-2 h-4"
+      <div className="flex flex-col lg:flex-row-reverse gap-4">
+        <div className="w-full col-span-2 h-80 lg:w-2/4">
+          <Preview data={previewData} />
+        </div>
+
+        <div className="grid grid-cols-2 gap-2 w-full lg:w-2/4">
+          <div className="dropdown col-span-2">
+            <label tabIndex={0} className="btn m-1 w-full">
+              {!type ? "Select job" : type}
+              <FontAwesomeIcon
+                icon={faChevronDown}
+                className="inline-block pl-2 h-4"
+              />
+            </label>
+
+            <ul
+              tabIndex={0}
+              className="bg-base-200 dropdown-content menu p-2 rounded-box w-full"
+            >
+              {[
+                "Generate monthly report",
+                "Generate yearly report",
+                "Archive data",
+                "Train ML model purple unicorn",
+              ].map((job) => (
+                <li key={job}>
+                  <a onClick={() => setType(job)}>{job}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className="col-span-1">
+            <label htmlFor="" className="label">
+              Earliest starting time
+            </label>
+            <DatePicker
+              className="input input-bordered w-full"
+              showTimeSelect
+              selected={startDate}
+              onChange={(date) => setStartDate(date)}
+              selectsStart
+              startDate={startDate}
+              endDate={endDate}
+              dateFormat="d/MM/yyyy hh:mmaa"
             />
-          </label>
+          </div>
 
-          <ul
-            tabIndex={0}
-            className="bg-base-200 dropdown-content menu p-2 rounded-box w-full"
-          >
-            {[
-              "Generate monthly report",
-              "Generate yearly report",
-              "Archive data",
-              "Train ML model purple unicorn",
-            ].map((job) => (
-              <li key={job}>
-                <a onClick={() => setType(job)}>{job}</a>
-              </li>
-            ))}
-          </ul>
-        </div>
-
-        <div className="col-span-1">
-          <label htmlFor="" className="label">
-            Earliest starting time
-          </label>
-          <DatePicker
-            className="input input-bordered w-full"
-            showTimeSelect
-            selected={startDate}
-            onChange={(date) => setStartDate(date)}
-            selectsStart
-            startDate={startDate}
-            endDate={endDate}
-            dateFormat="d/MM/yyyy hh:mmaa"
-          />
-        </div>
-
-        <div className="col-span-1">
-          <label htmlFor="" className="label">
-            Execute until
-          </label>
-          <DatePicker
-            className="input input-bordered w-full"
-            showTimeSelect
-            selected={endDate}
-            onChange={(date) => setEndDate(date)}
-            selectsEnd
-            startDate={startDate}
-            endDate={endDate}
-            minDate={startDate}
-            dateFormat="d/MM/yyyy hh:mmaa"
-          />
-        </div>
-
-        <div className="dropdown col-span-2">
-          <label tabIndex={0} className="btn m-1 w-full">
-            {repeat}
-            <FontAwesomeIcon
-              icon={faChevronDown}
-              className="inline-block pl-2 h-4"
+          <div className="col-span-1">
+            <label htmlFor="" className="label">
+              Execute until
+            </label>
+            <DatePicker
+              className="input input-bordered w-full"
+              showTimeSelect
+              selected={endDate}
+              onChange={(date) => setEndDate(date)}
+              selectsEnd
+              startDate={startDate}
+              endDate={endDate}
+              minDate={startDate}
+              dateFormat="d/MM/yyyy hh:mmaa"
             />
-          </label>
+          </div>
 
-          <ul
-            tabIndex={0}
-            className="bg-base-200 dropdown-content menu p-2 rounded-box w-full"
-          >
-            {[
-              "no repeat",
-              "repeat daily",
-              "repeat weekly",
-              "repeat monthy",
-              "repeat yearly",
-            ].map((repeat) => (
-              <li key={repeat}>
-                <a onClick={() => setRepeat(repeat)}>{repeat}</a>
-              </li>
-            ))}
-          </ul>
+          <div className="dropdown col-span-2">
+            <label tabIndex={0} className="btn m-1 w-full">
+              {repeat}
+              <FontAwesomeIcon
+                icon={faChevronDown}
+                className="inline-block pl-2 h-4"
+              />
+            </label>
+
+            <ul
+              tabIndex={0}
+              className="bg-base-200 dropdown-content menu p-2 rounded-box w-full"
+            >
+              {[
+                "no repeat",
+                "repeat daily",
+                "repeat weekly",
+                "repeat monthy",
+                "repeat yearly",
+              ].map((repeat) => (
+                <li key={repeat}>
+                  <a onClick={() => setRepeat(repeat)}>{repeat}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <button className="btn col-span-2 bg-primary text-neutral hover:bg-primary">
+            create
+          </button>
         </div>
-
-        <button className="btn col-span-2 bg-primary text-neutral hover:bg-primary">
-          create
-        </button>
       </div>
     </div>
   );
